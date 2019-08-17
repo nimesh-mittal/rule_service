aa76e7c6099b0a46051fcfccbd7f97ee53913767
Nimesh Mittal <nimesh.mittal@gmail.com>
2019-08-17 12:05:55 +0530

    1. Moving evaluator and ruleset under pkg

--- pkg/evaluator/constants.go
+++ pkg/evaluator/constants.go
@@ -23,12 +23,10 @@ const (
 var (
 	ErrorOperatorNotSupported   = errors.New("operator not supported")
 	ErrorFieldNotInRecord       = errors.New("field is missing in the record")
-	ErrorFieldTypeNotSupported  = errors.New("field type not supported")
 	ErrorUnableToInferFieldType = errors.New("unable to infer field type")
 	ErrorUnableToParseField     = errors.New("unable to parse field")
 	ErrorFailedToApplyOperator  = errors.New("failed to apply operator on values")
 	ErrorNumberCantnotBeNil     = errors.New("number can not be nil")
-	ErrorUnknownCause           = errors.New("unknown cause of this error")
 	ErrorEmptyMatch             = errors.New("unable to match with any rule")
 )
 
